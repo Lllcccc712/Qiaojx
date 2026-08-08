955082213b56eace459f6817de59ba75cb4d36bd
Lllcccc712 <1584750243@qq.com>
2026-08-08 11:06:11 +0800

    修改

--- keil_proj_demo-8.6-8.8H7/Core/Src/main.c
+++ keil_proj_demo-8.6-8.8H7/Core/Src/main.c
@@ -261,32 +261,32 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 /* 函数定义：这里才是函数的具体实现 */
-void blink_led(uint8_t led_num, uint16_t times, uint32_t delay_ms)
-{
-  uint16_t i = 0U; /* 循环计数变量 */
+// void blink_led(uint8_t led_num, uint16_t times, uint32_t delay_ms)
+// {
+//   uint16_t i = 0U; /* 循环计数变量 */
 
-  /* if 判断：LED 编号只允许 1~4 */
-  if (led_num > LED_COUNT)
-  {
-    return; /* return 直接结束当前函数 */
-  }
+//   /* if 判断：LED 编号只允许 1~4 */
+//   if (led_num > LED_COUNT)
+//   {
+//     return; /* return 直接结束当前函数 */
+//   }
 
-  /* for 循环：初始化; 判断条件; 每次循环后执行 */
-  for (i = 0U; i < times; i++)
-  {
-    led_on(led_num);          /* 点亮指定 LED */
-    HAL_Delay(delay_ms);      /* 延时一段时间 */
-    led_off(led_num);         /* 熄灭指定 LED */
-    HAL_Delay(delay_ms);
-  }
-}
+//   /* for 循环：初始化; 判断条件; 每次循环后执行 */
+//   for (i = 0U; i < times; i++)
+//   {
+//     led_on(led_num);          /* 点亮指定 LED */
+//     HAL_Delay(delay_ms);      /* 延时一段时间 */
+//     led_off(led_num);         /* 熄灭指定 LED */
+//     HAL_Delay(delay_ms);
+//   }
+// }
 
-void beep(uint32_t beep_ms)
-{
-  buzzer_on();          /* 打开蜂鸣器 */
-  HAL_Delay(beep_ms);   /* 保持响一段时间 */
-  buzzer_off();         /* 关闭蜂鸣器 */
-}
+// void beep(uint32_t beep_ms)
+// {
+//   buzzer_on();          /* 打开蜂鸣器 */
+//   HAL_Delay(beep_ms);   /* 保持响一段时间 */
+//   buzzer_off();         /* 关闭蜂鸣器 */
+// }
 /* USER CODE END 4 */
 
  /* MPU Configuration */
