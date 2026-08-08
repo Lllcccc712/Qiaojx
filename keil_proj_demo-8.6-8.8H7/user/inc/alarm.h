ec02de62128b40e502bf027530d638a4a316f6e9
Lllcccc712 <1584750243@qq.com>
2026-08-08 17:25:00 +0800

    blink_led() 和 beep() 从 main.c 移到 user/src、user/inc 目录下的新文件里封装

--- keil_proj_demo-8.6-8.8H7/user/inc/alarm.h
+++ keil_proj_demo-8.6-8.8H7/user/inc/alarm.h
@@ -4,8 +4,8 @@
 #include "main.h"  // 必须包含，因为要用到 uint8_t 等类型
 
 // 这里留空，准备写 3 个函数的声明
-void practice_blink_led(uint8_t led_num , uint8_t blink_times)
-// void beep(...);
-// void alarm_sys_func(...);
+void practice_blink_led(uint8_t led_num , uint8_t blink_times);
+void beep(uint32_t beep_ms);
+void alarm_sys_func(...);
 
 #endif
