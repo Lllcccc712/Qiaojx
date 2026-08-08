09deef559f5c89dea747e36b34acd551f37e9292
Lllcccc712 <1584750243@qq.com>
2026-08-08 20:02:09 +0800

    改

--- keil_proj_demo-8.6-8.8H7/user/src/alarm.c
+++ keil_proj_demo-8.6-8.8H7/user/src/alarm.c
@@ -3,11 +3,31 @@
 #include "buzzer.h"   /* 蜂鸣器驱动的函数声明 */
 #include "led.h"      /* LED 驱动的函数声明 */
 
+
 /* #define 是宏定义：编译前会把名字替换成后面的内容 */
 #define LED_COUNT   4U    /* 板载 LED 数量，U 表示 unsigned 无符号数 */
 #define BLINK_TIMES 5U    /* 每颗 LED 闪烁次数 */
 #define DELAY_MS    250U  /* LED 亮/灭持续时间，单位毫秒 */
 #define BEEP_MS     120U  /* 蜂鸣器响一声的时长 */
+
+
+void practice_blink_led(uint8_t led_num , uint8_t blink_times, uint32_t delay_ms)
+{
+
+
+  if (led_num > LED_COUNT)
+  {
+    return; /* return 直接结束当前函数 */
+  }
+  for(uint8_t i = 0; i < blink_times; i++)
+    {
+      led_on(led_num);          /* 点亮指定 LED */
+      HAL_Delay(500);      /* 延时一段时间 */
+      led_off(led_num);         /* 熄灭指定 LED */
+      HAL_Delay(500);
+  }
+}
+
 
 void alarm_sys_func(uint8_t led_num, uint8_t mode)
 {
@@ -28,7 +48,7 @@ void alarm_sys_func(uint8_t led_num, uint8_t mode)
             beep(BEEP_MS);
         }
         break;
-        
+
         default:
         break;
 
